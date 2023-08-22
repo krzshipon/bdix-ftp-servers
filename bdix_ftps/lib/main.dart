202012bdfe9eb4910d21769cfce2dc8b6767dc1d
krzshipon <khairuzzaman.shipon@bjitgroup.com>
2023-08-22 16:44:09 +0600

    [Icons]

--- bdix_ftps/lib/main.dart
+++ bdix_ftps/lib/main.dart
@@ -11,7 +11,7 @@ Future<void> main() async {
 
   runApp(
     GetMaterialApp(
-      title: 'app_name'.tr,
+      title: "BDIX FTPS",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       theme: appThemeL,
